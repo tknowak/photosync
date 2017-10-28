cc01317d2d546eab2bd24c7392daaa76298c761d
Tomasz Nowak <tomasz.nowak.tn1@contractors.roche.com>
2017-10-28 10:03:22 +0200

    Added Python application for moving RAW files if there is no corresponding jpg files anymore

---
+++ photosync.py
@@ -0,0 +1,23 @@
+import os
+
+print("Will move all RAW files, which doesn't have it's JPG file anymore")
+
+photos_dir = 'C:\\Foto\\London'
+raw_dir = photos_dir + '/./' + 'RAW'
+deleted_dir = raw_dir + "/deleted"
+jpg_file_format = 'JPG'
+raw_file_format = 'ORF'
+
+files = [file for file in os.listdir(photos_dir) if file.endswith("." + jpg_file_format)]
+file_numbers = [os.path.splitext(file)[0] for file in files]
+
+if not os.path.exists(deleted_dir):
+    os.makedirs(deleted_dir)
+
+raw_files = [raw_file for raw_file in os.listdir(raw_dir)]
+for raw_file in raw_files:
+    if raw_file.endswith("." + raw_file_format) and os.path.splitext(raw_file)[0] not in file_numbers:
+        print("Moving: " + raw_file)
+        os.rename(raw_dir + "/" + raw_file, deleted_dir + "/" + raw_file)
+
+print("Finished moving RAW files")
